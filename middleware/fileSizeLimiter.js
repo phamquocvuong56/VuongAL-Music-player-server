bc1911498023331fc611ddfffe9e9b181aea53ab
vuongpq <phamquocvuong56k2@gmail.com>
2023-02-22 21:11:35 +0700

    final

--- middleware/fileSizeLimiter.js
+++ middleware/fileSizeLimiter.js
@@ -1,17 +1,19 @@
-const MB= 10;
+const MB= 20;
 const FILE_SIZE_LIMIT = MB * 1024*1024;
 
 const fileSizeLimiter = (req, res, next) =>{
     const files= req.files
     const filesOverLimit = []
+    let sizeOver;
     Object.keys(files).forEach(key=>{
         if(files[key].size > FILE_SIZE_LIMIT){
             filesOverLimit.push(files[key].name)
+            sizeOver=files[key].size
         }
     })
     if(filesOverLimit.length){
         const properVerb= filesOverLimit.length>1?'are':'is'
-        const sentence= `Upload failed. ${filesOverLimit.toString()} ${properVerb} over the file size limit of ${MB} MB`.replaceAll(",", ", ")
+        const sentence= `Upload failed. ${filesOverLimit.toString()} ${properVerb} over the file size limit of ${MB} MB (${Math.floor(sizeOver/1024/1024)}MB)`.replaceAll(",", ", ")
         const message= filesOverLimit.length<3
         ? sentence.replace(",", " and")
         : sentence.replace(/,(?=[^,]*$)/, " and")
--- middleware/filesPayloadExists.js
+++ middleware/filesPayloadExists.js
@@ -1,5 +1,5 @@
 const filesPayloadExists= (req, res, next)=>{
-    if(!req.files) return res.status(400).json({status:'error', message: 'missing files'})
+    if(!req.files) return res.status(200).json({status:'error', message: 'missing files'})
     next()
 }
 module.exports= filesPayloadExists
